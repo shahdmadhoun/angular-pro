a0e6d78624adc374f4cd30f0bef3f9753614249c
shahdmadhoun <shahdem100@gmail.com>
2021-01-11 21:07:59 +0300

    add date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,17 @@ export class AppComponent {
   title = "my-app";
   studentName = "your name";
   studentAge = 0;
+  studentDate = new Date(1, 1, 2000);
   students = [
-    { name: "shahd", age: 20 },
-    { name: "malak", age: 15 },
-    { name: "ahmad", age: 25 }
+    { name: "shahd", age: 20, date: new Date(4, 14, 2000) },
+    { name: "malak", age: 15, date: new Date(1, 1, 1996) },
+    { name: "ahmad", age: 25, date: new Date(30, 12, 1990) }
   ];
   add() {
     this.students.push({
       name: this.studentName,
-      age: this.studentAge
+      age: this.studentAge,
+      date: this.studentDate
     });
   }
 }
