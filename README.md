5997d87a96ca63771fa345b345990fabd01c772e
shahdmadhoun <shahdem100@gmail.com>
2021-01-11 20:10:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vhuzua-edyvze
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vhuzua-edyvze)
---
+++ src/app/customers.ts
@@ -0,0 +1,16 @@
+import { Customer } from './customer';
+
+export const CUSTOMERS: Customer[] = [
+  { name: 'Maria' },
+  { name: 'Oliver' },
+  { name: 'Walter' },
+  { name: 'Lakshmi' },
+  { name: 'Yasha' }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
